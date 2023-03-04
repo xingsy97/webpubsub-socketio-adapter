e07cdb51e011b7d62bcfde5a2ccbb89fac20170e
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-03-04 21:30:51 +0800

    re-org file structure

--- samples/EIO/eio-server-wps.ts
+++ samples/EIO/eio-server-wps.ts
@@ -1,7 +1,8 @@
 process.env.DEBUG="*";
 import express from "express";
 import {Server as EioServer, Socket} from "G:/engine.io";
-import {WebPubSubServerAdapter, eioBuild} from "../../index";
+import {WebPubSubServerAdapter, eioBuild} from "../../";
+const path = require('path');
 const wpsOptions = {
   hub: "eio_hub", 
   path: "/eventhandler/",
@@ -9,6 +10,10 @@ const wpsOptions = {
 }
 
 const app = express();
+console.log(__dirname)
+var p = path.join(__dirname, "public").replace("build\\","")
+console.log(p);
+app.use(express.static(p) as any);
 
 const adapter = new WebPubSubServerAdapter(wpsOptions);
 const eioServer = new EioServer({transports:['websocket'], wsEngine: adapter, httpCompression:false, pingTimeout:1000000, pingInterval:1000});
