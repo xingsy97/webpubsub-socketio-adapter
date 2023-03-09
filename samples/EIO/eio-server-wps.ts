995cde7bb679c72d09359c7dd767b80247d0baad
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-03-09 21:19:13 +0800

    new API design

--- samples/EIO/eio-server-wps.ts
+++ samples/EIO/eio-server-wps.ts
@@ -1,7 +1,6 @@
 process.env.DEBUG="*";
 import express from "express";
-import {Server as EioServer} from "G:/engine.io";
-import {WebPubSubServerAdapter, eioBuild} from "../../src";
+import {WebPubSubEioManager} from "../../src";
 const path = require('path');
 
 const wpsOptions = {
@@ -9,11 +8,9 @@ const wpsOptions = {
   path: "/eventhandler/",
   connectionString: "Endpoint=http://localhost;Port=8080;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGH;Version=1.0;",
 }
-const adapter = new WebPubSubServerAdapter(wpsOptions);
 
 const eioOptions = {
   transports:['websocket'], 
-  wsEngine: adapter, 
   httpCompression:false, 
   pingInterval:5000
 }
@@ -21,13 +18,12 @@ const eioOptions = {
 const app = express();
 app.use(express.static(path.join(__dirname, "public").replace("build\\","")) as any);
 
-const eioServer = new EioServer(eioOptions as any);
-const httpServer = eioBuild(app, eioServer, wpsOptions);
+const eioManager = new WebPubSubEioManager(app, eioOptions as any, wpsOptions);
 
-eioServer.on("connection", socket => {
+eioManager.eioServer.on("connection", socket => {
   console.log("[SIO Server][onConnect]");
   socket.send("[From Server] hello");
   setInterval(() => { console.log(socket.readyState); }, 10000);
 });
 
-httpServer.listen(3000);
+eioManager.httpServer.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -209,9 +209,64 @@ class WebPubSubServerAdapter {
   And the Engine.IO handshake behaviour is inside `handleRequest`
   Web PubSub handshake handler is inside its express middleware. So a temporary ExpressApp and HttpServer are created as bridges to pass Web PubSub handler into Engine.IO middlewares.
 */
-function eioBuild(app: core.Express, eioServer: EioServer, wpsOptions: WebPubSubServerAdapterOptions): HttpServer {
+// function eioBuild(app: core.Express, eioServer: EioServer, wpsOptions: WebPubSubServerAdapterOptions): HttpServer {
+class WebPubSubEioManager {
+	public httpServer: HttpServer;
+	public eioServer: EioServer;
 
-	function addWebPubSubMiddlewaresToEIO(eioServer: EioServer) {
+	constructor(app: core.Express, opts: ServerOptions, wpsOptions: WebPubSubServerAdapterOptions) {
+		let adapter = new WebPubSubServerAdapter(wpsOptions);
+		var eioServer = new EioServer({...opts, wsEngine: adapter});
+
+		hookEioServer(eioServer);
+		this.addWebPubSubMiddlewaresToEIO(eioServer);
+
+		// Set negotiate handler for ExpressApp
+		app.get('/negotiate', async (req, res) => {
+			let id = req.query.id;
+			if (!id) {
+				res.status(400).send('missing user id');
+				return;
+			}
+			/*
+				tokenInfo = { 
+					"token": "A.B.C", "baseUrl": "http://localhost/clients/engineio/hubs/{hub}", "url": "http://localhost/clients/engineio/hubs/{hub}?access_token=A.B.C",
+					"eio": {
+						"baseUrl": "http://localhost",
+						"query": { "access_token": "A.B.C" }
+					}
+				}
+			*/
+			// TODO: getClientAccessToken is hard-encoded hoooked. Need soft hook
+			let tokenInfo = await ((eioServer as any).ws.serviceClient as WebPubSubServiceClient).getClientAccessToken({ userId: id });
+			let eioPath = `/clients/engineio/hubs/${wpsOptions.hub}`;
+			tokenInfo["eio"] = {
+				"baseUrl": tokenInfo.baseUrl.substring(0, tokenInfo.baseUrl.indexOf("/clients/engineio/hubs/")),
+				"query": {"access_token": tokenInfo.token},
+				"path": eioPath
+			}
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			console.log(tokenInfo);
+			res.json(tokenInfo);
+		});
+		const httpServer = new HttpServer(app);
+		(eioServer as any).ws.httpServer = httpServer;
+		
+		// In Engine.IO, method `init` is called both inside constructor [server.js:59] and `attach` [server.js:568] 
+		// Without this line, `init` will be called twice and cause re-run of `eioServer.wsEngine(...)` 
+		(eioServer as any).init = () => {};
+		eioServer.attach(httpServer, { 
+			path: wpsOptions.path,
+			addTrailingSlash:false
+		});
+
+		(eioServer as any).ws.linkedEioServer = eioServer;
+
+		this.httpServer = httpServer;
+		this.eioServer = eioServer;
+	}
+
+	addWebPubSubMiddlewaresToEIO(eioServer: EioServer) {
 		// Pass Web PubSub Express middlewares into Engine.IO middlewares
 		let bridgeApp = express();
 		bridgeApp.use((eioServer as any).ws.getMiddleware());
@@ -220,67 +275,21 @@ function eioBuild(app: core.Express, eioServer: EioServer, wpsOptions: WebPubSub
 		(eioServer as any).middlewares = bridgeHttpServer.listeners("request");	
 		bridgeHttpServer.removeAllListeners("request");
 	}
-
-	hookEioServer(eioServer);
-	addWebPubSubMiddlewaresToEIO(eioServer);
-
-	// Set negotiate handler for ExpressApp
-	app.get('/negotiate', async (req, res) => {
-		let id = req.query.id;
-		if (!id) {
-			res.status(400).send('missing user id');
-			return;
-		}
-		/*
-			tokenInfo = { 
-				"token": "A.B.C", "baseUrl": "http://localhost/clients/engineio/hubs/{hub}", "url": "http://localhost/clients/engineio/hubs/{hub}?access_token=A.B.C",
-				"eio": {
-					"baseUrl": "http://localhost",
-					"query": { "access_token": "A.B.C" }
-				}
-			}
-		*/
-		// TODO: getClientAccessToken is hard-encoded hoooked. Need soft hook
-		let tokenInfo = await ((eioServer as any).ws.serviceClient as WebPubSubServiceClient).getClientAccessToken({ userId: id });
-		let eioPath = `/clients/engineio/hubs/${wpsOptions.hub}`;
-		tokenInfo["eio"] = {
-			"baseUrl": tokenInfo.baseUrl.substring(0, tokenInfo.baseUrl.indexOf("/clients/engineio/hubs/")),
-			"query": {"access_token": tokenInfo.token},
-			"path": eioPath
-		}
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		console.log(tokenInfo);
-		res.json(tokenInfo);
-	});
-	const httpServer = new HttpServer(app);
-	(eioServer as any).ws.httpServer = httpServer;
-	
-	// In Engine.IO, method `init` is called both inside constructor [server.js:59] and `attach` [server.js:568] 
-	// Without this line, `init` will be called twice and cause re-run of `eioServer.wsEngine(...)` 
-	(eioServer as any).init = () => {};
-	eioServer.attach(httpServer, { 
-		path: wpsOptions.path,
-		addTrailingSlash:false
-	});
-
-	(eioServer as any).ws.linkedEioServer = eioServer;
-	return httpServer;
 }
 
 class WebPubSubSioManager{
-	httpServer: HttpServer;
-	eioServer: EioServer;
-	sioServer: SioServer;
-
+	public sioServer: SioServer;
+	private eioManager: WebPubSubEioManager;
+	
 	constructor(app: core.Express, wpsOptions: WebPubSubServerAdapterOptions, opts: ServerOptions & AttachOptions) {
-		let adapter = new WebPubSubServerAdapter(wpsOptions);
-
-		this.eioServer = new EioServer({...opts, wsEngine: adapter});
-		this.httpServer = eioBuild(app, this.eioServer, wpsOptions);
-
+		this.eioManager = new WebPubSubEioManager(app, opts, wpsOptions);
 		this.sioServer = new SioServer(null, {...opts, path: wpsOptions.path});
 		this.sioServer.bind(this.eioServer);
 	}
+
+	public get httpServer() : HttpServer { return this.eioManager.httpServer; }
+	public get eioServer() : EioServer { return this.eioManager.eioServer; }
+	
 }
 
 // function sioBuild(app: core.Express, sioServer: SioServer) {
@@ -294,4 +303,4 @@ class WebPubSubSioManager{
 // 	return httpServer;
 // }
 
-export {WebPubSubSioManager, WebPubSubServerAdapter, eioBuild};
+export {WebPubSubSioManager, WebPubSubEioManager};
